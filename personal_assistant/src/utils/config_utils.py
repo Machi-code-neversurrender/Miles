88bdbcfafeffd560fc02766f14e1ac2c45901b29
Machi-code-neversurrender <156617093+Machi-code-neversurrender@users.noreply.github.com>
2025-01-07 21:37:46 -0500

    Delete personal_assistant directory

--- personal_assistant/src/utils/config_utils.py
+++
@@ -1,5 +0,0 @@
-import yaml
-
-def load_config(config_file="config.yaml"):
-    with open(config_file, "r") as file:
-        return yaml.safe_load(file)
--- personal_assistant/src/web_scraping.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-class WebScraper:
-    def scrape(self, url):
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        headlines = [h.text.strip() for h in soup.find_all("h2", class_="headline")]
-        return headlines
